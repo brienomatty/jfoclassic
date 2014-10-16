547ec0a1d869bcdf3804ba9c9583ba01c03cc8e9
Matthew O'Brien <matthewobrien@matthews-mbp.home>
2014-10-15 20:04:26 -0400

    Initialize repository

---
+++ app/controllers/application_controller.rb
@@ -0,0 +1,7 @@
+class ApplicationController < ActionController::Base
+  # Prevent CSRF attacks by raising an exception.
+  # For APIs, you may want to use :null_session instead.
+  def hello
+  	render text: "hello, world!"
+  end
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Earbud::Application.config.secret_key_base = '885d03e3518726c3c9d6dbbf91baec0b1e64cbd8ffc8ec42051f60eaacce9098a93ff806a08fcf60c7a5ff3b4b3e602e199ddc2827908e06cd721d7c46aa416c'
