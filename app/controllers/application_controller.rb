d2b8320c9d0cd7c4d8889ef4515e1a84362068e7
Matthew O'Brien <matthewobrien@matthews-mbp.home>
2014-10-16 17:49:46 -0400

    Goodbye

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,12 @@
 class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
-  def hello
-  	render text: "hello, world!"
+  def hola
+  	render text: "hola, mundo!"
   end
+
+  def goodbye
+  	render text: "Goodbye, bitches!"
+  end
+
 end
