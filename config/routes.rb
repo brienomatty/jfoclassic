5db4f08f136c0d82dc668fea9a3038502e005cf5
Matthew O'Brien <matthewobrien@Matthews-MBP.fios-router.home>
2015-02-06 21:39:35 -0500

    Add Blog page

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,7 @@ Rails.application.routes.draw do
   get 'about'   => 'static_pages#about'
   get 'contact' => 'static_pages#contact'
   get 'signup'  => 'users#new'
+  get 'blog'    => 'blog#blog'
+  get 'joecool' => 'static_pages#joecool'
   resources :charges
 end
